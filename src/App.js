6dffa0059a9b4cbebc66d5bdeb5275a9abab85f6
lironezra <lironezra1@gmail.com>
2020-04-28 12:35:42 +0300

    added react-preloading-screen to improve UI while component is rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import { Preloader, Placeholder } from 'react-preloading-screen';
 
 import Header from './components/Header/header.component';
 import SlideShowBanner from './components/SlideShowBanner/slide-show-banner.component';
 import Features from './components/Features/features.component';
 import PhotoGallery from './components/PhotoGallery/photo-gallery.component';
 import Footer from './components/Footer/footer.component';
+import Spinner from './components/Shared/Spinner/spinner.component';
 
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <SlideShowBanner />
-      <Features />
-      <PhotoGallery />
-      <Footer />
-    </div>
+    <Preloader>
+      <div className="App">
+        <Header />
+        <SlideShowBanner />
+        <Features />
+        <PhotoGallery />
+        <Footer />
+      </div>
+
+      <Placeholder>
+        <Spinner />
+      </Placeholder>
+    </Preloader>
   );
 }
 
