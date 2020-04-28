b5c58ada3de7caeebc1be00b9991f72f837e86f9
lironezra <lironezra1@gmail.com>
2020-04-28 12:14:46 +0300

    added SlideShow, Features, PhotoGallery, Fotter components for App view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import Header from './components/Header/header.component';
+import SlideShowBanner from './components/SlideShowBanner/slide-show-banner.component';
+import Features from './components/Features/features.component';
+import PhotoGallery from './components/PhotoGallery/photo-gallery.component';
+import Footer from './components/Footer/footer.component';
+
 
 import './App.css';
 
@@ -8,6 +13,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <SlideShowBanner />
+      <Features />
+      <PhotoGallery />
+      <Footer />
     </div>
   );
 }
