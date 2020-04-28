40b1c8f49df3f32554b38fdfb38e8c46b44a5fb8
lironezra <lironezra1@gmail.com>
2020-04-28 12:10:11 +0300

    added login button at top of header

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import AppLogo from '../../assets/images/app-logo.png';
+import UserLogo from '../../assets/icons/user.png';
 
 import './header.styles.scss';
 
 const Header = () => {
     return (
         <header className="header-container">
+            <div className="login-user-link">
+                <img src={UserLogo} alt="user" />
+                <span>כניסה</span>
+            </div>
             <div className="top-wrapper">
                 <img src={AppLogo} alt="app-logo" />
                 <h1>שיצוידע - לאוהבי הגזע - האתר הרשמי</h1>
@@ -17,7 +22,15 @@ const Header = () => {
                         <ul>
                             <li>בית</li>
                             <li>קצת עלינו</li>
-                            <li>המלצות</li>
+                            <li className="dropdown">
+                                <a href="https://www.w3schools.com/howto/howto_css_dropdown.asp">המלצות</a>
+                                {/* <button class="dropbtn">Dropdown</button> */}
+                                <div className="dropdown-content">
+                                    <a className="link" href="https://www.w3schools.com/howto/howto_css_dropdown.asp">ספרים מומלצים</a>
+                                    <a className="link" href="https://www.w3schools.com/howto/howto_css_dropdown.asp">מוצרי טיפוח</a>
+                                    <a className="link" href="https://www.w3schools.com/howto/howto_css_dropdown.asp">אוכל מומלץ</a>
+                                </div>
+                            </li>
                             <li>אוכל</li>
                         </ul>
                     </div>
