8bdacf18daaabf2ef396e92854a53bdd43480f78
lironezra <lironezra1@gmail.com>
2020-04-30 17:36:29 +0300

    Updates

---
+++ precache-manifest.d6c39bdc503513435fd5e2831e1d30ca.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89f9cb0b0118fc9fe26ffc4c7a562e9d",
+    "url": "/shihTzuYeda/index.html"
+  },
+  {
+    "revision": "2ec233496e4a6a3e9917",
+    "url": "/shihTzuYeda/static/css/2.8730d5d4.chunk.css"
+  },
+  {
+    "revision": "79f8c7e6c9a988e7fe1e",
+    "url": "/shihTzuYeda/static/css/main.92fc52e5.chunk.css"
+  },
+  {
+    "revision": "2ec233496e4a6a3e9917",
+    "url": "/shihTzuYeda/static/js/2.7bc6c7b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shihTzuYeda/static/js/2.7bc6c7b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f8c7e6c9a988e7fe1e",
+    "url": "/shihTzuYeda/static/js/main.8740c7c1.chunk.js"
+  },
+  {
+    "revision": "9b3384aa561cd079ae75",
+    "url": "/shihTzuYeda/static/js/runtime-main.864315b0.js"
+  },
+  {
+    "revision": "5ba31aad1938430a36de02c865d1f635",
+    "url": "/shihTzuYeda/static/media/app-logo.5ba31aad.png"
+  },
+  {
+    "revision": "7cecbc8c456be84941f41bd42d237653",
+    "url": "/shihTzuYeda/static/media/shih-tzu-1.7cecbc8c.jpg"
+  },
+  {
+    "revision": "687cfe8e2d62f894c5c1558a03548561",
+    "url": "/shihTzuYeda/static/media/shih-tzu-10.687cfe8e.jpg"
+  },
+  {
+    "revision": "cf40245a57d91f563dd1dc8c4bfdbef7",
+    "url": "/shihTzuYeda/static/media/shih-tzu-11.cf40245a.jpg"
+  },
+  {
+    "revision": "7bc5b09fffa318bf894be8867ae09252",
+    "url": "/shihTzuYeda/static/media/shih-tzu-12.7bc5b09f.jpg"
+  },
+  {
+    "revision": "b274924cd20552732937f8204474a3e8",
+    "url": "/shihTzuYeda/static/media/shih-tzu-2.b274924c.jpg"
+  },
+  {
+    "revision": "7dd5b253d23f826e804c996f4f0cc31e",
+    "url": "/shihTzuYeda/static/media/shih-tzu-3.7dd5b253.jpg"
+  },
+  {
+    "revision": "25c7c5ead7c1e7062490d6901de5e02a",
+    "url": "/shihTzuYeda/static/media/shih-tzu-6.25c7c5ea.jpg"
+  },
+  {
+    "revision": "7b9b113cd7a7001f4867d308554deb20",
+    "url": "/shihTzuYeda/static/media/shih-tzu-7.7b9b113c.jpg"
+  },
+  {
+    "revision": "c2cf083af1b8b833044110b0742c98fc",
+    "url": "/shihTzuYeda/static/media/shih-tzu-8.c2cf083a.jpg"
+  },
+  {
+    "revision": "0a50faea725b5f2aaea7eb041894497e",
+    "url": "/shihTzuYeda/static/media/shih-tzu-9.0a50faea.jpg"
+  },
+  {
+    "revision": "da0505b18894fc11b2e43fe8e5e529b3",
+    "url": "/shihTzuYeda/static/media/shihtzh-transparant2.da0505b1.png"
+  },
+  {
+    "revision": "49b79c5c736af866cc7aa0b050d5ceeb",
+    "url": "/shihTzuYeda/static/media/shihtzh-transparant3.49b79c5c.png"
+  },
+  {
+    "revision": "f2f384ce8591971417b45323da4000af",
+    "url": "/shihTzuYeda/static/media/shihtzu-running.f2f384ce.jpg"
+  },
+  {
+    "revision": "93410869c910eb22e881d4c20480f934",
+    "url": "/shihTzuYeda/static/media/shihtzu-trans5.93410869.png"
+  },
+  {
+    "revision": "0f1a5c2a681966dbf490c2c9a5a4c92e",
+    "url": "/shihTzuYeda/static/media/user-profile-icon-6.0f1a5c2a.jpg"
+  }
+]);
