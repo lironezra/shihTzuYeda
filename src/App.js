2f1e4216bee1caf45fe6d9ac1cfaac35431600b0
lironezra <lironezra1@gmail.com>
2020-05-14 09:52:26 +0300

    using SideDrawer component for mobile devices

--- src/App.js
+++ src/App.js
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Preloader, Placeholder } from 'react-preloading-screen';
 
 import HomePage from './pages/Home/home.component';
 import AboutUsPage from './pages/AboutUs/about-us.component';
+import FoodPage from './pages/Food/food.component';
 
 import Header from './components/Header/header.component';
+import SideDrawer from './components/SideDrawer/side-drawer.component';
 import Footer from './components/Footer/footer.component';
-import Spinner from './components/Shared/Spinner/spinner.component';
-
+import Loader from './components/Shared/Loader/loader.component';
+import Backdrop from './components/Shared/BackDrop/backdrop.component';
 
 import './App.css';
 
 function App() {
+  let backDrop;
+  const [sideDrawerOpen, setSideDrawerOpen] = useState(false);
+
+  const drawerToggleClickHandler = () => {
+    setSideDrawerOpen(!sideDrawerOpen);
+  }; 
+
+  const backDropClickHandler = () => {
+    setSideDrawerOpen(false);
+  };
+
+  if (sideDrawerOpen) {
+    backDrop = <Backdrop click={backDropClickHandler}/>
+  }
+
   return (
     <Preloader>
       <div className="App">
-        <Header />
+        <Header drawerClickHandler={drawerToggleClickHandler}/>
+        <SideDrawer visible={sideDrawerOpen} closeSideDrawer={backDropClickHandler}/>
+        {backDrop}
 
         <Switch>
-          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/shihTzuYeda' component={HomePage}/>
           <Route path='/about-us' component={AboutUsPage}/>
+          <Route path='/food' component={FoodPage} />
         </Switch>
 
-        {/* <HomePage /> */}
         <Footer />
       </div>
 
       <Placeholder>
-        <Spinner />
+        <Loader />
       </Placeholder>
     </Preloader>
   );
