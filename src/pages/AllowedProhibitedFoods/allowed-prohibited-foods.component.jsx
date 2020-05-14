fb2b3d3c9a31f5033f61348c34a2f525b56ae743
lironezra <lironezra1@gmail.com>
2020-05-14 12:29:34 +0300

    added  AllowedProhibitedFoods component

---
+++ src/pages/AllowedProhibitedFoods/allowed-prohibited-foods.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './allowed-prohibited-foods.styles.scss';
+
+const AllowedProhibitedFoodsPage = () => {
+    return (
+        <div className="allowed-prohibited-foods-page-container">
+            <div className="section-header">
+                <h1>מאכלים מותרים, אסורים וכאלה עם מינון</h1>
+            </div>
+            <div className="collection-items-container">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default AllowedProhibitedFoodsPage;
