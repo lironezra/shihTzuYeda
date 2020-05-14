06cd4d21c7baadfbcf0aea14728d28bdd1d675fb
lironezra <lironezra1@gmail.com>
2020-05-14 09:44:37 +0300

    added Loader component for Preloading screen

---
+++ src/components/Shared/Loader/loader.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './loader.styles.scss';
+
+const Loader = () => {
+    return (
+        <div className="preloader">
+            <div className="spinner">
+                <div className="double-bounce1"></div>
+                <div className="double-bounce2"></div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Loader;
