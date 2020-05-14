972bc2379905481923e7e6c8cd2a5f43ee9abe3e
lironezra <lironezra1@gmail.com>
2020-05-14 10:53:29 +0300

    passing features data dynamicly

--- src/pages/Home/home.component.jsx
+++ src/pages/Home/home.component.jsx
@@ -4,11 +4,38 @@ import SlideShowBanner from '../../components/SlideShowBanner/slide-show-banner.
 import Features from '../../components/Features/features.component';
 import PhotoGallery from '../../components/PhotoGallery/photo-gallery.component';
 
+import SalonIcon from '../../assets/icons/salon.png';
+import FoodIcon from '../../assets/icons/canned-food.png';
+import BalanceIcon from '../../assets/icons/balance.png';
+
+const features = [ 
+    {
+        icon: SalonIcon, 
+        headerText: "טיפוח שיצו",
+        descText: "כולנו יודעים כמה חשוב לטפח אותם"
+    },
+    {
+        icon: FoodIcon, 
+        headerText: "מזון שיצו",
+        descText: "כי הם לא רק מפונקים הם גם בררניים"
+    },
+    {
+        icon: BalanceIcon, 
+        headerText: "השוואת ביטוחים",
+        descText: "כדי שנשים לב לאותיות הקטנות"
+    }
+];
+
+
 const HomePage = () => {
     return (
         <>
             <SlideShowBanner />
-            <Features />
+            <Features 
+                features={features}
+                title="יש לנו את כל מה שהשיצו שלכם צריך"
+                description="מגוון רחב של קטגוריות והמלצות למתוקים שלנו"
+            />
             <PhotoGallery />
         </>
     );
