7e3ae7ebb35f918aefc03db23ab8f066216c9704
lironezra <lironezra1@gmail.com>
2020-07-06 11:33:11 +0300

    fix routing handler while nevigate from home page

--- src/components/Features/feature-card.component.jsx
+++ src/components/Features/feature-card.component.jsx
@@ -1,29 +1,33 @@
-import React from 'react'
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 import './feature-card.styles.scss';
 
 const FeatureCard = (props) => {
-    const { icon, headerText, descText, route, history, match } = props;
+  const { icon, headerText, descText, route, history, match } = props;
 
-    const handleClick = () => {
-        history.push(`${match.path}/${route}`);
+  const handleClick = () => {
+    if (match.path === '/shihTzuYeda') {
+      history.push(`/${route}`);
+    } else {
+      history.push(`${match.path}/${route}`);
     }
+  };
 
-    return (
-        <div className="feature-card">
-            <div className="card-icon">
-                <img src={icon} alt="salon" />
-            </div>
-            <div className="card-content">
-                <h2>{headerText}</h2>
-                <p>{descText}</p>
-            </div>
-            <div className="go-to-content">
-                <button onClick={handleClick}>קרא עוד >></button>
-            </div>
-        </div>
-    );
+  return (
+    <div className='feature-card'>
+      <div className='card-icon'>
+        <img src={icon} alt='salon' />
+      </div>
+      <div className='card-content'>
+        <h2>{headerText}</h2>
+        <p>{descText}</p>
+      </div>
+      <div className='go-to-content'>
+        <button onClick={handleClick}>קרא עוד {'>>'}</button>
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(FeatureCard);
