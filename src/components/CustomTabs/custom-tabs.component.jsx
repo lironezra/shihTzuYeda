11ba454920821070e81e4ccac16aba7d701202d2
lironezra <lironezra1@gmail.com>
2020-07-06 11:10:19 +0300

    save cahnges

--- src/components/CustomTabs/custom-tabs.component.jsx
+++ src/components/CustomTabs/custom-tabs.component.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   makeStyles,
   createMuiTheme,
-  MuiThemeProvider,
-} from "@material-ui/core/styles";
+  MuiThemeProvider
+} from '@material-ui/core/styles';
 
-import AppBar from "@material-ui/core/AppBar";
-import TabPanel from "../Shared/TabPanel/tab-panel.component";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
+import AppBar from '@material-ui/core/AppBar';
+import TabPanel from '../Shared/TabPanel/tab-panel.component';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-import FoodCard from "../FoodCard/food-card.component";
+import FoodCard from '../FoodCard/food-card.component';
 
-import "./custom-tabs.styles.scss";
+import './custom-tabs.styles.scss';
 
 function a11yProps(index) {
   return {
     id: `full-width-tab-${index}`,
-    "aria-controls": `full-width-tabpanel-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`
   };
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.paper,
-    width: "100%",
-  },
+    width: '100%'
+  }
 }));
 
 const CustomTabs = ({ tabsName, tabPanels }) => {
@@ -33,8 +33,8 @@ const CustomTabs = ({ tabsName, tabPanels }) => {
 
   const theme = createMuiTheme({
     typography: {
-      fontFamily: "'Varela Round', sans-serif",
-    },
+      fontFamily: "'Varela Round', sans-serif"
+    }
   });
 
   const [value, setValue] = useState(0);
@@ -43,23 +43,19 @@ const CustomTabs = ({ tabsName, tabPanels }) => {
     setValue(newValue);
   };
 
-  //   const handleChangeIndex = (index) => {
-  //     setValue(index);
-  //   };
-
   return (
     <MuiThemeProvider theme={theme}>
       <div className={classes.root}>
-        <AppBar position="static" color="default">
+        <AppBar position='static' color='default'>
           <Tabs
             value={value}
             onChange={handleChange}
-            indicatorColor="primary"
-            textColor="primary"
-            variant="fullWidth"
+            indicatorColor='primary'
+            textColor='primary'
+            variant='fullWidth'
           >
             {tabsName.map((name, i) => (
-              <Tab key={i} label={name} className="tab" {...a11yProps(i)} />
+              <Tab key={i} label={name} className='tab' {...a11yProps(i)} />
             ))}
           </Tabs>
         </AppBar>
@@ -67,7 +63,7 @@ const CustomTabs = ({ tabsName, tabPanels }) => {
           ? tabPanels.map((tp, i) => {
               return (
                 <TabPanel key={i} value={value} index={i}>
-                  <div className="collection-items-container">
+                  <div className='collection-items-container'>
                     {tp.map((item, i) => (
                       <FoodCard key={i} image={item.img} name={item.name} />
                     ))}
