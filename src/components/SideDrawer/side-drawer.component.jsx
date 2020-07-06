e95d4bf9e79f5184ebd34c7b6ea9627087b8c0d9
lironezra <lironezra1@gmail.com>
2020-07-06 11:36:42 +0300

    added team-events link

--- src/components/SideDrawer/side-drawer.component.jsx
+++ src/components/SideDrawer/side-drawer.component.jsx
@@ -1,49 +1,78 @@
-import React from 'react';
+import React from "react";
 
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
-import UserLogo from '../../assets/icons/user.png';
+import UserLogo from "../../assets/icons/user.png";
 
-import './side-drawer.styles.scss';
-
+import "./side-drawer.styles.scss";
 
 const SideDrawer = ({ visible, closeSideDrawer }) => {
-    let drawerClasses = ['side-drawer'];
+  let drawerClasses = ["side-drawer"];
 
-    if (visible) {
-        drawerClasses = ['side-drawer', 'visible'];
-    }
-    
-    return (
-        <nav className={drawerClasses.join(' ')}>
-            <div className={`login-user-link`}>
-                <img src={UserLogo} alt="user" />
-                <span>כניסה</span>
-            </div>
-            <ul>
-                <li>
-                    <NavLink className='side-drawer-option' activeClassName='is-active' to='/shihTzuYeda' onClick={closeSideDrawer}>
-                        בית                   
-                    </NavLink>   
-                </li>
-                <li>
-                    <NavLink className='side-drawer-option' activeClassName='is-active' to='/about-us' onClick={closeSideDrawer}>
-                        קצת עלינו                   
-                    </NavLink>   
-                </li>
-                <li>
-                    <NavLink className='side-drawer-option' activeClassName='is-active' to='/recomendations' onClick={closeSideDrawer}>
-                        המלצות                   
-                    </NavLink> 
-                </li>
-                <li>
-                    <NavLink className='side-drawer-option' activeClassName='is-active' to='/food' onClick={closeSideDrawer}>
-                        אוכל                   
-                    </NavLink> 
-                </li>
-            </ul>
-        </nav>
-    );
-} 
+  if (visible) {
+    drawerClasses = ["side-drawer", "visible"];
+  }
+
+  return (
+    <nav className={drawerClasses.join(" ")}>
+      <div className={`login-user-link`}>
+        <img src={UserLogo} alt="user" />
+        <span>כניסה</span>
+      </div>
+      <ul>
+        <li>
+          <NavLink
+            className="side-drawer-option"
+            activeClassName="is-active"
+            to="/shihTzuYeda"
+            onClick={closeSideDrawer}
+          >
+            בית
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="side-drawer-option"
+            activeClassName="is-active"
+            to="/about-us"
+            onClick={closeSideDrawer}
+          >
+            קצת עלינו
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="side-drawer-option"
+            activeClassName="is-active"
+            to="/team-events"
+            onClick={closeSideDrawer}
+          >
+            אירועים
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="side-drawer-option"
+            activeClassName="is-active"
+            to="/recomendations"
+            onClick={closeSideDrawer}
+          >
+            המלצות
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="side-drawer-option"
+            activeClassName="is-active"
+            to="/food"
+            onClick={closeSideDrawer}
+          >
+            אוכל
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default SideDrawer;
