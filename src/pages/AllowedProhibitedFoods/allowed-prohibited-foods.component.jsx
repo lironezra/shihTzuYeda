c949f00f781a8a3a1129e1d6df668ce3c16b658b
lironezra <lironezra1@gmail.com>
2020-07-06 11:40:10 +0300

    added dummy data for dispaly some food types

--- src/pages/AllowedProhibitedFoods/allowed-prohibited-foods.component.jsx
+++ src/pages/AllowedProhibitedFoods/allowed-prohibited-foods.component.jsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React from "react";
 
-import './allowed-prohibited-foods.styles.scss';
+import CustomTabs from "../../components/CustomTabs/custom-tabs.component";
+
+import "./allowed-prohibited-foods.styles.scss";
+
+const TABS_DATA = {
+  tabsName: ["מותרים", "מותרים עם מינון", "אסורים "],
+  // Each TabPanel is set of food images
+  tabPanels: [
+    [
+      {
+        name: "מנגו",
+        img:
+          "https://res.cloudinary.com/dwylnsnmk/image/upload/q_auto/v1589973598/shih-tzu-yeda/Food/mango_lwjdso.jpg",
+      },
+      {
+        name: "בקר,עוף, הודו, כבש, דגים",
+        img:
+          "https://res.cloudinary.com/dwylnsnmk/image/upload/q_auto/v1589973598/shih-tzu-yeda/Food/meat_lwys2e.jpg",
+      },
+    ],
+    [
+      {
+        name: "פסטה",
+        img:
+          "https://res.cloudinary.com/dwylnsnmk/image/upload/q_auto/v1589973671/shih-tzu-yeda/Food/pasta_hxbxrz.jpg",
+      },
+    ],
+    [
+      {
+        name: "בצל",
+        img:
+          "https://res.cloudinary.com/dwylnsnmk/image/upload/q_auto/v1589973671/shih-tzu-yeda/Food/onions_sxqr1j.jpg",
+      },
+      {
+        name: "שום",
+        img:
+          "https://res.cloudinary.com/dwylnsnmk/image/upload/q_auto/v1589973672/shih-tzu-yeda/Food/garlic_inz5fu.jpg",
+      },
+    ],
+  ],
+};
 
 const AllowedProhibitedFoodsPage = () => {
-    return (
-        <div className="allowed-prohibited-foods-page-container">
-            <div className="section-header">
-                <h1>מאכלים מותרים, אסורים וכאלה עם מינון</h1>
-            </div>
-            <div className="collection-items-container">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="allowed-prohibited-foods-page-container">
+      <CustomTabs
+        tabsName={TABS_DATA.tabsName}
+        tabPanels={TABS_DATA.tabPanels}
+      />
+    </div>
+  );
 };
 
 export default AllowedProhibitedFoodsPage;
